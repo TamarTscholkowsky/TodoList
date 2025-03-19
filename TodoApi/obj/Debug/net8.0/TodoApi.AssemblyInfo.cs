0cfde4402e29bc588e8903d35995a14c66c9e6d5
unknown <TT0527123469@gmail.com>
2025-03-19 18:35:35 +0200

    addstringconction3

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3110033789395a2cec153d7e67d59736bd245c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b566937ed44ad8747905701da5bb116ab515a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
