eb361bbf253309d126aac3c76572bb50d038142f
unknown <TT0527123469@gmail.com>
2025-03-20 19:05:43 +0200

    vclient9

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // require('dotenv').config();
 // axios.defaults.baseURL 
-axios.defaults.baseURL =process.env.APP_API_URL
+axios.defaults.baseURL =process.env.REACT_APP_API_URL
 // const apiUrl = 'https://todolistserver1-uu5v.onrender.com';
 //response
 // הוספת אינטרספטור לתגובות
