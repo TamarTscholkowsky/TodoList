e55dd07168d3511799d78910aec35ae652538687
TamarTscholkowsky <TT0527123469@gmail.com>
2025-03-24 20:29:50 +0200

    Update service.jstry

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 require('dotenv').config();
 // require('dotenv').config();
 // axios.defaults.baseURL 
-axios.defaults.baseURL ="https://todolistserver1-uu5v.onrender.com"
+axios.defaults.baseURL ='https://todolistserver1-uu5v.onrender.com'
 // const apiUrl = 'https://todolistserver1-uu5v.onrender.com';
 //response
 // הוספת אינטרספטור לתגובות
@@ -22,7 +22,7 @@ export default {
   getTasks: async () => {
     debugger
     try {
-      const result = await axios.get(`/tasks`)
+      const result = await axios.get(`https://todolistserver1-uu5v.onrender.com/tasks`)
       return result.data;
     }
     catch (err) {
