8303230f7130955d6a5f73c5d1b31d8ec1d8a9f0
unknown <TT0527123469@gmail.com>
2025-03-24 21:33:55 +0200

    vvvvtry

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-
-
 // require('dotenv').config();
 // axios.defaults.baseURL 
 
 axios.defaults.baseURL ='https://todolistserver1-uu5v.onrender.com'
-const apiUrl = 'https://todolistserver1-uu5v.onrender.com';
+// const apiUrl = 'https://todolistserver1-uu5v.onrender.com';
 //response
 // הוספת אינטרספטור לתגובות
 axios.interceptors.response.use(response => {
