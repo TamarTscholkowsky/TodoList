2b12e08f076ece7f0004aacef88a911adbf4bf0c
TamarTscholkowsky <TT0527123469@gmail.com>
2025-03-24 19:32:58 +0200

    Update server.jstt

--- render-node-app/server.js
+++ render-node-app/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
   try {
-    const { data } = await axios.get("https://api-docs.render.com/reference/list-services", {
+    const { data } = await axios.get("https://todolistrender-node.onrender.com", {
       headers: { Authorization: `Bearer ${RENDER_API_KEY}` },
     });
     res.json(data);
