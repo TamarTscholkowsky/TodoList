d57da6d7437693ce10db0a0403b5935df328777b
TamarTscholkowsky <TT0527123469@gmail.com>
2025-03-24 19:47:05 +0200

    Update service.js

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 require('dotenv').config();
 // require('dotenv').config();
 // axios.defaults.baseURL 
-axios.defaults.baseURL =process.env.REACT_APP_API_URL
+axios.defaults.baseURL ="https://todolistserver1-uu5v.onrender.com"
 // const apiUrl = 'https://todolistserver1-uu5v.onrender.com';
 //response
 // הוספת אינטרספטור לתגובות
